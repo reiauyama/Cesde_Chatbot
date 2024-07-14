19078d1af70af46c148329145657c3a8aa6fedc7
reiauyama <leosanmo_01-@outlook.es>
2024-07-14 17:59:08 -0500

    Se ha organizado mejor los scripts para el web scraping y se ha optimizado su rendimiento

--- chatbotProject/scraper/management/commands/scrape.py
+++ chatbotProject/scraper/management/commands/scrape.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from scraper.scraper import scrape_website
-from scraper.scraperWho import scrape_who_we_are
+
 from scraper.scraperProgramasSedes import scrape_sedes
 
 class Command(BaseCommand):
@@ -8,6 +8,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         scrape_website()
-        scrape_who_we_are()
+        
         scrape_sedes()
         self.stdout.write(self.style.SUCCESS('Successfully scraped the website'))
--- chatbotProject/scraper/scraper.py
+++ chatbotProject/scraper/scraper.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from .models import Menu, SubMenu, SubSubMenu, sub3menu
+from .models import Menu, SubMenu
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -46,19 +46,7 @@ def scrape_navigation(menu_id, ul_class):
                     defaults={'link': subhref, 'active': True}
                 )
 
-                subsubmenu_ul = subitem.find('ul')
-                if subsubmenu_ul:
-                    subsubmenu_items = subsubmenu_ul.find_all('li', recursive=False)
-                    for subsubitem in subsubmenu_items:
-                        subsub_link = subsubitem.find('a')
-                        subsub_title = subsub_link.get_text(strip=True) if subsub_link else None
-                        subsub_href = subsub_link['href'] if subsub_link and 'href' in subsub_link.attrs else None
-
-                        SubSubMenu.objects.update_or_create(
-                            submenu=submenu_entry,
-                            title=subsub_title,
-                            defaults={'link': subsub_href, 'active': True}
-                        )
+                
 
 def scrape_social_networks(soup):
     # Buscar el contenedor de Redes Sociales
@@ -100,7 +88,7 @@ def scrape_website():
     # Desactivar todas las entradas existentes antes de actualizar
     Menu.objects.all().update(active=False)
     SubMenu.objects.all().update(active=False)
-    SubSubMenu.objects.all().update(active=False)
+    
 
     url = "https://www.cesde.edu.co/"
     response = requests.get(url)
@@ -116,105 +104,5 @@ def scrape_website():
     # Eliminar las entradas que no se actualizaron (que ya no están activas)
     Menu.objects.filter(active=False).delete()
     SubMenu.objects.filter(active=False).delete()
-    SubSubMenu.objects.filter(active=False).delete()
 
-def update_or_create_entry(model, filter_kwargs, update_kwargs):
-    try:
-        entry, created = model.objects.update_or_create(
-            **filter_kwargs,
-            defaults=update_kwargs
-        )
-        return entry, created
-    except Exception as e:
-        logger.error(f"Error al actualizar o crear entrada en {model.__name__}: {e}")
-        return None, False
 
-def scrape_location(url, location_title):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    programas_acordeon = soup.find('div', class_='programas__accordeon')
-    if not programas_acordeon:
-        logger.warning(f"No se encontró el acordeón de programas en la URL: {url}")
-        return
-
-    programas = programas_acordeon.find_all('article', class_='programas__accordeon__item')
-
-    submenu_entry = SubMenu.objects.filter(title=location_title).first()
-    if not submenu_entry:
-        logger.warning(f"No se encontró el SubMenu con el título '{location_title}'")
-        return
-
-    # Crear un diccionario para rastrear las entradas procesadas
-    processed_subsubmenus = {}
-    processed_sub3menus = {}
-
-    for programa_item in programas:
-        extract = programa_item.find('h3')
-        subsub_title = extract.get_text(strip=True) if extract else 'Sin título'
-        logger.info(f"Procesando SubSubMenu: {subsub_title}")
-
-        # Actualizar o crear subsubmenu_entry
-        subsubmenu_entry, created = update_or_create_entry(
-            SubSubMenu,
-            {'submenu': submenu_entry, 'title': subsub_title},
-            {'link': ' ', 'active': True}
-        )
-        if not subsubmenu_entry:
-            continue
-        logger.info(f"{'Creado' if created else 'Actualizado'} SubSubMenu: {subsub_title}")
-
-        # Agregar al diccionario de procesados
-        processed_subsubmenus[subsubmenu_entry.id] = subsubmenu_entry
-
-        content_extract = programa_item.find('div', class_='text__content')
-        content = content_extract.get_text(strip=True) if content_extract else 'Sin contenido'
-        escuela_extract = programa_item.find('h4')
-        escuela = escuela_extract.get_text(strip=True) if escuela_extract else 'Sin escuela'
-        link = programa_item.find('a')['href'] if programa_item.find('a') else ''
-        logger.info(f"Procesando Sub3Menu: {escuela} - {content} - {link}")
-
-        # Actualizar o crear sub3menu_entry
-        sub3menu_entry, created = update_or_create_entry(
-            sub3menu,
-            {'subsubmenu': subsubmenu_entry, 'escuela': escuela},
-            {'content': content, 'link': link, 'active': True}
-        )
-        if not sub3menu_entry:
-            continue
-        logger.info(f"{'Creado' if created else 'Actualizado'} Sub3Menu: {escuela} - {content} - {link}")
-
-        # Agregar al diccionario de procesados
-        processed_sub3menus[sub3menu_entry.id] = sub3menu_entry
-
-    # Desactivar las entradas no procesadas
-    SubSubMenu.objects.filter(submenu=submenu_entry).exclude(id__in=processed_subsubmenus.keys()).update(active=False)
-    sub3menu.objects.filter(subsubmenu__submenu=submenu_entry).exclude(id__in=processed_sub3menus.keys()).update(active=False)
-
-    # Eliminar las entradas no activas
-    SubSubMenu.objects.filter(submenu=submenu_entry, active=False).delete()
-    sub3menu.objects.filter(subsubmenu__submenu=submenu_entry, active=False).delete()
-def scrape_sedes():
-    def scrape_medellin():
-        scrape_location("https://www.cesde.edu.co/sedes/medellin/", "Medellín")
-    scrape_medellin()
-
-    def scrape_bello():
-        scrape_location("https://www.cesde.edu.co/sedes/bello/", "Bello")
-    scrape_bello()
-
-    def scrape_rionegro():
-        scrape_location("https://www.cesde.edu.co/sedes/rionegro/", "Rionegro")
-    scrape_rionegro()
-
-    def scrape_pintada():
-        scrape_location("https://www.cesde.edu.co/sedes/pintada/", "La Pintada")
-    scrape_pintada()
-
-    def scrape_apartado():
-        scrape_location("https://www.cesde.edu.co/sedes/apartado/", "Apartadó")
-    scrape_apartado()
-
-    def scrape_bogota():
-        scrape_location("https://www.cesde.edu.co/sedes/bogota/", "Bogotá")
-    scrape_bogota()
